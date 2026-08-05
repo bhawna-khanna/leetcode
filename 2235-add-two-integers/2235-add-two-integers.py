55384fc40cd6d2deac4d105c6d29744f3b525ebb
bhawna-khanna <bhawnakhanna002@gmail.com>
2026-08-05 16:34:41 +0530

    Time: 0 ms (100%), Space: 19.3 MB (13.63%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        self.num1=num1
+        self.num2=num2
+        return num1+num2
+
+
+s1=Solution()
+s1.sum(23,56)
+
+        
